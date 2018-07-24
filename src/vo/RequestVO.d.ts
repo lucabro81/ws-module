4884c0c536a2bd5e706369c2b80b009f5fc6cd96
Luca <lucabro_2000@yahoo.it>
2018-07-24 14:41:48 +0200

    prova aggiunta index.ts

--- src/vo/RequestVO.d.ts
+++ src/vo/RequestVO.d.ts
@@ -1,16 +1,15 @@
-import {EndPointVO} from "./EndPointVO";
 import {Signal} from "signals";
 import {WarningLevel} from "../utils/Emun";
 
 export interface RequestVO {
-    endpoint:EndPointVO,
-    method?:string, // FIXME: il method potrebbe essere assegnato dal decorato @Get o @Post
-    data?:any,
-    config?:any,
-    error_signals?:Array<Signal>,
-    error_intercept?:boolean,
-    error_callback?:() => void,
-    warning_level_override?:WarningLevel,
-    retry_override?:number,
-    debounce_override?:number
+    endpoint: any, // sarebbe EndPointVO ma preferisco generarla per ogni progetto
+    method?: string,
+    data?: any,
+    config?: any,
+    error_signals?: Array<Signal>,
+    error_intercept?: boolean,
+    error_callback?: () => void,
+    warning_level_override?: WarningLevel,
+    retry_override?: number,
+    debounce_override?: number
 }
